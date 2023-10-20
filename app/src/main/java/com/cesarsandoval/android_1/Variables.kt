6a569ccba0e1b79bffa79a6645cf7e32bc8351ae
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-10-20 10:45:20 -0300

    2%

--- app/src/main/java/com/cesarsandoval/android_1/Variables.kt
+++ app/src/main/java/com/cesarsandoval/android_1/Variables.kt
@@ -1,8 +1,19 @@
 package com.cesarsandoval.android_1
 
-//VARIABLES
+//VARIABLES NUMERICAS
 
 fun main() {
-    val name = "cesar"
+
+    //Int
+    val age: Int = 42
+
+    //Long
+    val example:Long = 33554411122122587
+
+    //Float
+    val floatExample:Float = 30.5f
+
+    //Double
+    val doubleExample: Double = 3216.123456
 
 }
