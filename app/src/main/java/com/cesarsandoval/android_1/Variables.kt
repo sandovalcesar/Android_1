c13592ce9a2572e51d5c359e7f719db5b69ce725
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-10-20 11:42:50 -0300

    4%

--- app/src/main/java/com/cesarsandoval/android_1/Variables.kt
+++ app/src/main/java/com/cesarsandoval/android_1/Variables.kt
@@ -5,7 +5,8 @@ package com.cesarsandoval.android_1
 fun main() {
 
     //Int
-    val age: Int = 42
+    val age1: Int = 42
+    var age2: Int = 30
 
     //Long
     val example:Long = 33554411122122587
@@ -29,6 +30,25 @@ fun main() {
     val booleanExample1: Boolean = true
     val booleanExample2: Boolean = false
 
-    print(stringExamplre)
+    //print(stringExamplre)
+/**
+    println("sumar")
+    println(age1 + age2)
+
+    println("restar")
+    println(age1 - age2)
+
+    println("multiplicar")
+    println(age1 * age2)
+
+    println("dividir")
+    println(age1 / age2)
+
+    println("resto")
+    println(age1 % age2)**/
+
+    var stringConcatenada: String = "César"
+
+    println("Hola me llamo $stringConcatenada y tengo $age1")
 
 }
